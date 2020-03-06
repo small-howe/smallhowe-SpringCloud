2b408749490a83af9f661d9f9a5f1ad3ef7bd44a
samll-howe <2678691035@qq.com>
2020-03-06 10:25:36 +0800

    'init'

---
+++ eureka/src/test/java/com/tangwh/eureka/EurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tangwh.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
