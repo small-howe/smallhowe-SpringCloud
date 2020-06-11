3c0e0dbb658f0299766cd7c4923979b28ebb990a
samll-howe <2678691035@qq.com>
2020-06-11 09:26:53 +0800

    openfeign---分布式调用服务

---
+++ openfeign/src/main/java/com/tangwh/controller/HelloController.java
@@ -0,0 +1,38 @@
+package com.tangwh.controller;
+
+import com.tangwh.User;
+import com.tangwh.serivce.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import static java.net.URLEncoder.*;
+
+@RestController
+public class HelloController {
+
+
+    @Autowired
+    HelloService helloService;
+
+
+    /**
+     * 调用这个 /hello  先去 helloService
+     * helloService 然后去 服务命 为 provider中 调用 名字为 hello的接口
+     * @return
+     */
+    @GetMapping("/hello")
+    public String hello() throws UnsupportedEncodingException {
+        String hello2 = helloService.hello2("唐同学");
+        System.out.println(hello2);
+        User user = helloService.addUser(new User(1, "javaboy", "123"));
+        System.out.println(user);
+        helloService.deleteUserById(1);
+        helloService.getUserByName(encode("唐同学", "UTF-8"));
+        return helloService.hello();
+    }
+
+}
