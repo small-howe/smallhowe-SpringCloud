1ec73a2803c5075694561710dd069f9b36358a9c
samll-howe <2678691035@qq.com>
2020-06-24 14:16:11 +0800

    config-server-配置中心服务端

---
+++ config-server/src/main/java/com/tangwh/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.tangwh;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 由于 conFig-server接口 中添加了security保护
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and().cors().disable();
+    }
+}
