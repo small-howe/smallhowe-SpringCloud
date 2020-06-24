03d626deb01b62bd44b68d44771eb698e0e72aaf
samll-howe <2678691035@qq.com>
2020-06-24 17:08:12 +0800

    alibaba-nacos-sentinel-使用Nocos配置Sentinel流控

---
+++ alibaba-nacos-sentinel/src/main/java/com/tangwh/AlibabaNacosSentinelApplication.java
@@ -0,0 +1,14 @@
+package com.tangwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaNacosSentinelApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaNacosSentinelApplication.class, args);
+    }
+
+}
